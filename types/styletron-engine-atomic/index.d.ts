6ab7c5c08c48a9a481c101ac55d8b560ecbadab1
Bogdan Savluk <bogdan.savluk@uber.com>
2021-11-08 03:26:21 +0100

    update types for styletron-engine-atomic
    used types generated from styletron converted to ts
    added fixes to make tests pass

--- types/styletron-engine-atomic/index.d.ts
+++ types/styletron-engine-atomic/index.d.ts
@@ -1,85 +1,109 @@
-// Type definitions for styletron-engine-atomic 1.1
+// Type definitions for styletron-engine-atomic 1.4
 // Project: https://github.com/styletron/styletron
 // Definitions by: Jhey Tompkins <https://github.com/jh3y>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.9
 
-import { KeyframesObject, FontFace as FontFaceObject, StandardEngine, StyleObject } from 'styletron-standard';
+import { StandardEngine, KeyframesObject, FontFaceObject, StyleObject } from 'styletron-standard';
 
-export class SequentialIDGenerator {
+declare class SequentialIDGenerator {
     prefix: string;
     count: number;
     offset: number;
     msb: number;
     power: number;
-    constructor(prefix: string);
+    constructor(prefix?: string);
     next(): string;
     increment(): number;
-  }
-
-export class Cache<T> {
-    cache: {[key: string]: string};
-    idGenerator: SequentialIDGenerator;
-    key: string;
-    onNewValue: (cache: Cache<T>, id: string, value: any) => any;
-    constructor(
-      idGenerator: SequentialIDGenerator,
-      onNewValue: (cache: Cache<T>, id: string, value: any) => any,
-    );
-    addValue(key: string, value: T): number;
 }
-export class MultiCache<T> {
-    caches: {[key: string]: Cache<T>};
+
+type OnNewCacheFn<T> = (c: string, b: Cache<T>, a?: string | null) => any;
+type OnNewValueFn<T> = (cache: Cache<T>, id: string, value: T) => any;
+declare class MultiCache<T> {
+    caches: {
+        [x: string]: Cache<T>;
+    };
     idGenerator: SequentialIDGenerator;
-    onNewCache: (key: string, cache: Cache<T>, insertAtIndex: number) => any;
-    onNewValue: (cache: Cache<T>, id: string, value: T) => any;
+    onNewCache: OnNewCacheFn<T>;
+    onNewValue: OnNewValueFn<T>;
     sortedCacheKeys: string[];
-    constructor(
-      idGenerator: SequentialIDGenerator,
-      onNewCache: () => any,
-      onNewValue: () => any,
-    );
+    constructor(idGenerator: SequentialIDGenerator, onNewCache: OnNewCacheFn<T>, onNewValue: OnNewValueFn<T>);
     getCache(key: string): Cache<T>;
     getSortedCacheKeys(): string[];
 }
-
-export type hydrateType = HTMLCollectionOf<HTMLStyleElement> | HTMLStyleElement[] | NodeListOf<HTMLStyleElement>;
-export interface Sheet {
-  css: string;
-  attrs: { [key: string]: string };
+declare class Cache<T> {
+    cache: {
+        [x: string]: string;
+    };
+    idGenerator: SequentialIDGenerator;
+    key: string;
+    onNewValue: (cache: Cache<T>, id: string, value: any) => any;
+    constructor(idGenerator: SequentialIDGenerator, onNewValue: (cache: Cache<T>, id: string, value: any) => any);
+    addValue(key: string, value: T): string;
 }
 
-export interface ClientOptions {
-  hydrate?: hydrateType | undefined;
-  container?: Element | undefined;
-  prefix?: string | undefined;
+type hydrateT = HTMLCollectionOf<HTMLStyleElement> | HTMLStyleElement[] | NodeListOf<HTMLStyleElement>;
+interface optionsT$1 {
+    hydrate?: hydrateT;
+    container?: Element;
+    prefix?: string;
 }
-
-export interface ServerOptions {
-  prefix?: string | undefined;
-}
-
-export class Client implements StandardEngine {
-    constructor(opts?: ClientOptions);
-    styleElements: { [key: string]: HTMLStyleElement };
+declare class StyletronClient implements StandardEngine {
+    container: Element;
+    styleElements: {
+        [x: string]: HTMLStyleElement;
+    };
     fontFaceSheet: HTMLStyleElement;
     keyframesSheet: HTMLStyleElement;
-    styleCache: MultiCache<{pseudo: string, block: string}>;
+    styleCache: MultiCache<{
+        pseudo: string;
+        block: string;
+    }>;
     keyframesCache: Cache<KeyframesObject>;
     fontFaceCache: Cache<FontFaceObject>;
+    constructor(opts?: optionsT$1);
     renderStyle(style: StyleObject): string;
-    renderKeyframes(keyframes: KeyframesObject): string;
     renderFontFace(fontFace: FontFaceObject): string;
+    renderKeyframes(keyframes: KeyframesObject): string;
 }
-export class Server implements StandardEngine {
-    constructor(opts?: ServerOptions)
-    styleRules: { [key: string]: string };
+
+interface attrsT {
+    'data-hydrate'?: 'keyframes' | 'font-face';
+    media?: string;
+    class?: string;
+}
+interface sheetT {
+    css: string;
+    attrs: attrsT;
+}
+interface optionsT {
+    prefix?: string;
+}
+declare class StyletronServer implements StandardEngine {
+    styleCache: MultiCache<{
+        pseudo: string;
+        block: string;
+    }>;
+    keyframesCache: Cache<KeyframesObject>;
+    fontFaceCache: Cache<FontFaceObject>;
+    styleRules: {
+        [x: string]: string;
+    };
     keyframesRules: string;
     fontFaceRules: string;
-    getStylesheets(): Sheet[];
+    constructor(opts?: optionsT);
+    renderStyle(style: StyleObject): string;
+    renderFontFace(fontFace: FontFaceObject): string;
+    renderKeyframes(keyframes: KeyframesObject): string;
+    getStylesheets(): sheetT[];
     getStylesheetsHtml(className?: string): string;
     getCss(): string;
-    renderStyle(style: StyleObject): string;
-    renderKeyframes(keyframes: KeyframesObject): string;
-    renderFontFace(fontFace: FontFaceObject): string;
 }
+
+declare global {
+    interface Window {
+        __STYLETRON_DEVTOOLS__: any;
+    }
+}
+
+export { StyletronClient as Client, StyletronServer as Server };
